c304a5eb66a0628419ab630971e3ab46f721ab0c
Anastasiia <anastasia.halk27@gmail.com>
2019-02-06 16:16:12 +0300

    add

--- cookYourBrackfast/brackfasts/brackfasts/NewBreakTableViewController.swift
+++ cookYourBrackfast/brackfasts/brackfasts/NewBreakTableViewController.swift
@@ -8,14 +8,53 @@
 
 import UIKit
 
-class NewBreakTableViewController: UITableViewController {
-
+class NewBreakTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    
+    @IBAction func toggleIsVisitegPressed(_ sender: UIButton) {
+        if sender == yesButton{
+            sender.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            noButton.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        } else {
+        sender.backgroundColor = #colorLiteral(red: 1, green: 0.1035235793, blue: 0.07981744581, alpha: 1)
+        yesButton.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        }
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        if nameTextField.text == "" || addressTextField.text == "" || typeTextField.text == ""{
+            print("not all string full")
+        } else{
+            performSegue(withIdentifier: "unwindSegueFromBr", sender: self)
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        noButton.backgroundColor = #colorLiteral(red: 1, green: 0.1035235793, blue: 0.07981744581, alpha: 1)
+        yesButton.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
 
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
+    }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0{
@@ -39,6 +78,7 @@ class NewBreakTableViewController: UITableViewController {
     func chooseImagePickerAction(source: UIImagePickerController.SourceType) {
         if UIImagePickerController.isSourceTypeAvailable(source){
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             self.present(imagePicker, animated: true, completion: nil)
