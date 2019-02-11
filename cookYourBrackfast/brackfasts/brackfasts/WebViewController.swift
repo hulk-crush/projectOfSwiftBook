df8080765032e36000e5a130934bda370b736081
Anastasiia <anastasia.halk27@gmail.com>
2019-02-11 17:39:43 +0300

    файлы для айклауда и тд

---
+++ cookYourBrackfast/brackfasts/brackfasts/WebViewController.swift
@@ -0,0 +1,58 @@
+//
+//  WebViewController.swift
+//  brackfasts
+//
+//  Created by Admin on 11/02/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var url: URL!
+    var progressView: UIProgressView!
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        
+        toolbarItems = [progressButton, spacer, refreshButton]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+}
