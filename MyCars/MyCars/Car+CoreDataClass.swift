46c4841b42177ebe1175258bc0234f20276843a3
Anastasiia <anastasia.halk27@gmail.com>
2019-02-13 13:15:52 +0300

    new little projects

---
+++ MyCars/MyCars/Car+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Car+CoreDataClass.swift
+//  MyCars
+//
+// Created by Admin on 12/02/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Car)
+public class Car: NSManagedObject {
+
+}
