0fac5b9323a52aa4bff324f926976ad6d4d0de06
Anastasiia <anastasia.halk27@gmail.com>
2019-02-14 18:33:43 +0300

    new project, GCD

---
+++ GCD/GCD/SecondViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Admin on 14/02/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://pp.userapi.com/c834103/v834103754/194ae7/QK5Lq2MwP-8.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            self.image = UIImage(data: imageData)
+        
+        }
+}
---
+++ GCD/GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Admin on 14/02/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
