e6eceaaea80f23e20c91a70108882f911dd1f48b
Anastasiia <anastasia.halk27@gmail.com>
2019-03-22 13:09:29 +0300

    add textures, didSimulatePhysics

--- WarFly/WarFly/GameScene.swift
+++ WarFly/WarFly/GameScene.swift
@@ -8,10 +8,14 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion //доступ к сенсорам телефона
 
 class GameScene: SKScene {
     
-   
+    let motionManager = CMMotionManager() //доступ к акселерометру
+    var xAcceleration: CGFloat = 0
+    var player: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         
         let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -32,5 +36,29 @@ class GameScene: SKScene {
             let cloud = Cloud.populateSprite(at: CGPoint(x: x, y: y))
             self.addChild(cloud)
         }
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
+        
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3 //убавление значения акселерометра
+            }
+        }
     }
+    
+    override func didSimulatePhysics() {
+        super.didSimulatePhysics()
+        
+        player.position.x += xAcceleration * 50
+        
+        if player.position.x < -70 {
+            player.position.x = self.size.width + 70
+        } else if player.position.x > self.size.width + 70 {
+            player.position.x = -70
+        }
+        
+    }
+    
 }
---
+++ WarFly/WarFly/PlayerPlane.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerPlane.swift
+//  WarFly
+//
+//  Created by Admin on 22/03/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import SpriteKit
+
+class PlayerPlane: SKSpriteNode {
+    static func populate(at point: CGPoint) -> SKSpriteNode {
+        let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
+        let playerPlane = SKSpriteNode(texture: playerPlaneTexture)
+        playerPlane.setScale(0.5)
+        playerPlane.position = point
+        playerPlane.zPosition = 20
+        
+        return playerPlane
+    }
+}
